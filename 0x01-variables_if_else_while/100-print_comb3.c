b272780066dc63e1c19e5015648436a481c249d1
sultkid <abdulkabirsultan@gmail.com>
2022-04-11 11:57:15 +0000

    add 2 advance task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			if (i == j)
+			{}
+			else
+			{
+				putchar(i);
+				putchar(j);
+				if (i == 56 && j == 57)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
