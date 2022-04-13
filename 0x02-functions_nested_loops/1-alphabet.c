026129b171f8de8bbefff7ff416be7c03386d926
sultkid <abdulkabirsultan@gmail.com>
2022-04-13 22:48:41 +0000

    add task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,19 @@
-okay
+#include "main.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		print_alphabet(_putchar(x));
+	}
+	_putchar('\n);
+	return (0);
+}
