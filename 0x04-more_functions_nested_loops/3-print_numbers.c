c6a99e9aa668237f1a5f214fde40a8b938e06d51
sultkid <abdulkabirsultan@gmail.com>
2022-04-14 23:03:43 +0000

    add task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int c;
+
+	while (c >= 48 && c <= 57)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
