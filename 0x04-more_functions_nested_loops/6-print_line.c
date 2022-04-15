834ab4fb68a98fdc51316e36419f7db892f55bf2
sultkid <abdulkabirsultan@gmail.com>
2022-04-15 22:24:28 +0000

    add task 6 & 9

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,28 @@
-#include "main"
-
+#include "main.h"
 /**
- * print_line - print_line
- * @n: parameter
+ * print_line - draws a straight line in the terminal.
+ * @n: print int
+ * Return: Always 0.
  */
-
 void print_line(int n)
 {
-	if (n <= 0)
+
+
+	int i;
+
+	if (n > 0)
 	{
-		_putchar('\n');
+		for (i = 0; i < n; i++)
+		{
+			_putchar (95);
+		}
+
+		_putchar ('\n');
 	}
+
 	else
 	{
-		int i;
-
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+		_putchar ('\n');
 	}
+
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,37 +1,46 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: 0 (success)
+ *  * main - entry point
+ *   *
+ *    * Return: void
  */
 
 int main(void)
 {
+
+	int p = 100;
 	int i;
 
 	i = 1;
-
-	while (i <= 100)
+	while (i <= p)
 	{
-
-		if ((i % 5 == 0) && (i % 3 == 0))
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
+
 		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
+
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			if (i < p)
+				printf("Buzz ");
+
+			else
+				printf("Buzz");
 		}
+
 		else
 		{
-			printf("%d", i);
+			printf("%i ", i);
 		}
-		printf(" ");
+
 		i++;
+
+
 	}
 	printf("\n");
 	return (0);
