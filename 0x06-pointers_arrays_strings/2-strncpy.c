8f1898c011d4b2a66ebce9a5d6dbd57c63be38c9
sultkid <abdulkabirsultan@gmail.com>
2022-04-23 17:14:05 +0000

    add task 0 to 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - main
+ * @dest: member
+ * @src: member
+ * @n: member
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	while (i = 0; src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
