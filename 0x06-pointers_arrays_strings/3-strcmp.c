96a12ed6b73123a036b3bf2ef741252c838db4f5
sultkid <abdulkabirsultan@gmail.com>
2022-04-24 16:57:40 +0000

    fix task 2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,10 +8,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (s1 != '\0' && s2 != '\0' && s1 == s2)
+	char *str_one = s1;
+	char *str_two = s2;
+
+	while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
 	{
-		s1++;
-		s2++;
+		str_one++;
+		str_two++;
 	}
-	return (s1 - s2);
+	return (*str_one - *str_two);
 }
