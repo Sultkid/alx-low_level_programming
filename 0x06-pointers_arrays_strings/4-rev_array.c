9ed5ecb3782f806d9c63a4edd09749ffe1f7c0b4
sultkid <abdulkabirsultan@gmail.com>
2022-04-25 20:14:53 +0000

    add task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - main
+ * @a: member
+ * @n: number of items in array
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, i, j;
+
+	for (i = 0; j = n - 1; i < j; i++; j--)
+	{
+		tmp = *(a + i);
+		*(a + i) = *(a + j);
+		*(a + j) = tmp;
+	}
+}
