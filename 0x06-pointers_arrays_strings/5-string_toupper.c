dbca10efb1e041a44dbcc0c965558aef2e8ac6f4
sultkid <abdulkabirsultan@gmail.com>
2022-04-25 20:43:01 +0000

    add task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - main
+ * @s: member
+ * Return: s
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; i < s[i]; i++)
+	{
+		if (s[i] >= 'a' || s[i] <= 'z')
+		{
+			char d = s[i] - 32;
+		}
+	}
+	return (d);
+}
