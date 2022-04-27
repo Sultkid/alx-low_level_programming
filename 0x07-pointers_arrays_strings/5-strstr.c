fd10d1bf1c897af19600eeb3545d906598f1cb97
sultkid <abdulkabirsultan@gmail.com>
2022-04-27 20:42:48 +0000

    fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,23 +5,22 @@
  * @needle: member
  * Return: 0
  */
-char *_strstr(char *haystack, char *needle)
+char  *_strstr(char *haystack, char *needle)
 {
-	int i, j;
-
-	for (i = 0; i < haystack[i]; i++)
+	char *str1, *str2; /*Declaring variables*/
+	while (*haystack != '\0')
 	{
-		if (haystack[i] != '\0')
+		str1 = haystack;
+		str2 = needle;
+		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
 		{
-			for (j = 0; j < needle[j]; j++)
-			{
-				if ( needle[j] == haystack[i])
-				{
-					return (needle);
-					break;
-				}
-			}
+			haystack++;
+			str2++;
 		}
+		if (*str2 == '\0')
+		{
+			return (str1);
+		}
+		haystack = str1 + 1;
 	}
 	return (0);
-}
