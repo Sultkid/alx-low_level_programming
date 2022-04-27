1dce2eb464e9e1f0143448ebda70317030a56128
sultkid <abdulkabirsultan@gmail.com>
2022-04-27 12:50:34 +0000

    add task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - main function
+ * @c: member
+ * @s: member
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i < *s[i]; i++)
+	{
+		if (*s[i] != '\0' && *s[i] == c)
+		{
+			continue;
+		}
+	}
+	return (s);
+}
