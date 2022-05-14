87340e3117beccd732a589386addb3d8289f2add
sultkid <abdulkabirsultan@gmail.com>
2022-05-14 10:59:37 +0000

    task fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
 #include "function_pointers.h"
 /**
- * print_name - function 
+ * print_name - function
  * @name: name
- * @f - pointer to function
+ * @f: pointer to function
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	void (*myFunc)(char *) = &f;
+	void (*myFunc)(char *) = f;
+
 	myFunc(name);
 }
