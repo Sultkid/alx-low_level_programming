7c738f3d9f36d6340bbbd953b94b9c3e93b64faa
sultkid <abdulkabirsultan@gmail.com>
2022-05-31 12:39:05 +0100

    initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert bin to unit
+ * @b: bin to be converted
+ * Return: converted value
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i])
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			num <<= 1;
+			num += b[i] - '0';
+		}
+		else
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (num);
+}
